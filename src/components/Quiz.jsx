5d20ac4506574d2e45b13bcc2551c03e99726920
suparnMarathe <suparnm98@gmail.com>
2023-12-05 20:24:08 +0530

    moved logic to the components where it is actually needed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,33 +4,17 @@ import QUESTION from "../questions";
 import Question from "./Question";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTION.length;
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered");
-      setUserAnswers((prevUserAnswer) => {
-        return [...prevUserAnswer, selectedAnswer];
-      });
-      setTimeout(() => {
-        console.log(activeQuestionIndex);
-        console.log(selectedAnswer);
-        if (selectedAnswer === QUESTION[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-          console.log(answerState);
-        } else {
-          setAnswerState("wrong");
-        }
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  },
+  []);
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
     [handleSelectAnswer]
@@ -47,12 +31,9 @@ export default function Quiz() {
     <div id="quiz">
       <Question
         key={activeQuestionIndex}
-        answers={QUESTION[activeQuestionIndex].answers}
-        questionText={QUESTION[activeQuestionIndex].text}
+        index={activeQuestionIndex}
         onSelectAnswer={handleSelectAnswer}
         handleSkipAnswer={handleSkipAnswer}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
-        answerState={answerState}
       />
     </div>
   );
